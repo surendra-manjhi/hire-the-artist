20aab163551ef470edb51fcadad7c045817443ae
Surendra <manjhisurendra08@gmail.com>
2024-03-04 05:28:29 +0530

    Minor add

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,21 +1,26 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true,
+const userSchema = mongoose.Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
 	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 const User = mongoose.model("User", userSchema);
 
