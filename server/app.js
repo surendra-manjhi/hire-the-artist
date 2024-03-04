b0220db6d3de044c6184729575ddcd86f0ba7c27
Surendra <manjhisurendra08@gmail.com>
2024-03-04 17:35:10 +0530

    Create auth api route

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
-app.use("/api/v1", userRouter);
+app.use(express.json());
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/auth", authRouter);
 
 export default app;
